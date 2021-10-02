efdf4a8eb7aaade9fc2ddbaf18adafcaf6d6276e
Lukas Leindals <s183920@student.dtu.dk>
2021-10-02 18:43:50 +0200

    pip install from requirments file

--- run_scripts/setup.sh
+++ run_scripts/setup.sh
@@ -22,14 +22,14 @@ fi
 
 ### Make python environment
 module load python3 # only use if on HPC
-python3 -m venv AutoVC-env
+# python3 -m venv AutoVC-env
+python3 -m venv test-env
 
-source AutoVC-env/bin/activate
+# source AutoVC-env/bin/activate
+source test-env/bin/activate
 
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
-# python -m pip install torch==1.4.0 sklearn tqdm librosa torchvision==0.5.0 webrtcvad scipy matplotlib pandas seaborn wavenet_vocoder 
-# python -m pip install numba==0.49.1 ###numba==0.43.0
 
 deactivate
 
