872c49efe6ca9842f3df124080327adad8608a1e
Lukas Leindals <s183920@student.dtu.dk>
2021-10-03 13:57:24 +0200

    test scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,24 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-WaveRNN="1dtkRFL83Iya1wBt0ucBBb0Q4i5GlU6IF"
+model_type="$1"
+while getopts n: flag
+do
+    case "${flag}" in
+        n) model_name=${OPTARG};;
+    esac
+done
 
-SpeakerEncoder="1j-M5KoqvJWJINJLXyhz403gTuZTm4kwV"
-
-AutoVC_basic="1jKTxQUhBXNVi38C43YuDIg46QdAPgGrH"
-AutoVC_origin="1Pjhk-lb9QW4EzsUSzlpns0NOIKrHoBlU"
-AutoVC_SMK="1npx7nzdVapSbZg5wkHFFGbSvUPLstF_A"
-AutoVC_seed40_200k="1ovdribZLkx1Wky5IHEt2_1Ibo9AI3jbt"
+echo "Type: $model_type"
+echo "name: $model_name"
 
 
-# choice=$( echo "$1" )
-# echo $choice
-# onsider dict: https://www.xmodulo.com/key-value-dictionary-bash.html
-
-if [ "$1" = "WaveRNN" ]; then
-    gdown $WaveRNN --id -O "models/WaveRNN/"
-    # gdown $WaveRNN --id
-elif [ "$1" = "SpeakerEncoder" ]; then
-    gdown $SpeakerEncoder --id -O "models/SpeakerEncoder/"
-else
-    echo "\e[31mError: $1 is not a valid model name to download\e[0m"
-fi
+# while getopts u:a:f: flag
+# do
+#     case "${flag}" in
+#         u) username=${OPTARG};;
+#         a) age=${OPTARG};;
+#         f) fullname=${OPTARG};;
+#     esac
+# done
+# echo "Username: $username";
+# echo "Age: $age";
+# echo "Full Name: $fullname";
