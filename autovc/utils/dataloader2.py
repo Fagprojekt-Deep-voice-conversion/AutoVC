da45a6e0a8cf82d4972b1d47ae55698ddc34e6b8
Peter Grønning <s183922@win.dtu.dk>
2022-01-03 14:43:33 +0100

    Dataloader - zero padding for spectograms of unequal lengths

--- autovc/utils/dataloader2.py
+++ autovc/utils/dataloader2.py
@@ -5,14 +5,23 @@ from torch.utils.data import DataLoader, Dataset
 from autovc.preprocessing.preprocess_wav import WaveRNN_Mel
 from autovc.speaker_encoder.inference import *
 from autovc.speaker_encoder.audio import *
-
+from torch.nn.functional import pad
 class DataLoaderAutoVC(Dataset):
+    '''
+    A Data Loader class for training AutoVC.
+    Takes a path to a folder with data (.wav files) and makes a generator object (data loader) 
+    which batches spectorgrams of utterances and the corresponding speaker identiy embeddings.
+    
+    If spectograms in batch are of unequal size the smaller are padded with zeros to match the size of the largest.
+    '''
+
     def __init__(self, data_dir_path, speaker_encoder_path = 'Models/SpeakerEncoder/SpeakerEncoder.pt', device = 'cpu'):
         super(DataLoaderAutoVC, self).__init__()
 
-
+        # Initialise the Speaker Identiy Encoder for embeddings.
         _model = load_model(weights_fpath = speaker_encoder_path, device = device)
 
+        # Load wav files. Create spectograms and embeddings
         self.wav_files = [os.path.join(dirpath, filename) for dirpath , _, directory in os.walk(data_dir_path) for filename in directory]
 
         self.mel_spectograms = [torch.from_numpy(WaveRNN_Mel(wav)) for wav in self.wav_files]
@@ -25,10 +34,14 @@ class DataLoaderAutoVC(Dataset):
         return self.mel_spectograms[index], self.embeddings[index]
 
     def collate_fn(self, batch):
+        '''
+        Pad with zeros if spectrograms are of unequal sizes.
+        '''
         spectrograms, embeddings = zip(*batch)
-        min_length = min([spectogram.size(-1) for spectogram in spectrograms])
-
-        return torch.stack([spectogram[:,:min_length] for spectogram in spectrograms]), torch.stack(embeddings)
+        max_length = max([spectogram.size(-1) for spectogram in spectrograms]) 
+        padded_spectrograms = [pad(spectogram, (1, max_length - spectogram.size(-1))) for spectogram in spectrograms]
+        
+        return torch.stack(padded_spectrograms), torch.stack(embeddings)
 
     def data_loader(self, batch_size=1, shuffle=False,  num_workers=0, pin_memory=False, **kwargs):
         return torch.utils.data.DataLoader(
