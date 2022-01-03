202c008fa0536e7412f114f3bd776c7e80b0e3c0
Peter Grønning <s183922@win.dtu.dk>
2022-01-03 17:30:22 +0100

    Speaker encoder model class

--- autovc/speaker_encoder/model.py
+++ autovc/speaker_encoder/model.py
@@ -9,6 +9,7 @@ from scipy.optimize import brentq
 from torch import nn
 import numpy as np
 import torch
+from autovc.speaker_encoder.audio import *
 
 class SpeakerEncoder(nn.Module):
     """
@@ -20,25 +21,32 @@ class SpeakerEncoder(nn.Module):
     Not consistenet with AutoVC but the best i could find...
     Trained on GE2E loss for 1.5 M step
     """
-    def __init__(self, device, loss_device):
+    def __init__(self, device = None, **kwargs):
         super().__init__()
-        self.loss_device = loss_device
+        if device is None:
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        elif isinstance(device, str):
+            self.device = torch.device(device)
         
         # Network defition
-        self.lstm = nn.LSTM(input_size=hp.mel_n_channels,
-                            hidden_size=hp.model_hidden_size,
-                            num_layers=hp.model_num_layers,
-                            batch_first=True).to(device)
-        self.linear = nn.Linear(in_features=hp.model_hidden_size,
-                                out_features=hp.model_embedding_size).to(device)
-        self.relu = torch.nn.ReLU().to(device)
+        self.lstm = nn.LSTM(input_size  = kwargs.get('mel_n_channels', hp.mel_n_channels),
+                            hidden_size = kwargs.get('model_hidden_size', hp.model_hidden_size),
+                            num_layers  = kwargs.get('model_num_layers', hp.model_num_layers),
+                            batch_first = True
+                            ).to(self.device)
+
+        self.linear = nn.Linear(in_features  = kwargs.get('model_hidden_size', hp.model_hidden_size),
+                                out_features = kwargs.get('model_embedding_size', hp.model_embedding_size),
+                                ).to(self.device)
+
+        self.relu = torch.nn.ReLU().to(self.device)
         
         # Cosine similarity scaling (with fixed initial parameter values)
-        self.similarity_weight = nn.Parameter(torch.tensor([10.])).to(loss_device)
-        self.similarity_bias = nn.Parameter(torch.tensor([-5.])).to(loss_device)
+        self.similarity_weight = nn.Parameter(torch.tensor([10.])).to(self.device)
+        self.similarity_bias = nn.Parameter(torch.tensor([-5.])).to(self.device)
 
         # Loss
-        self.loss_fn = nn.CrossEntropyLoss().to(loss_device)
+        self.loss_fn = nn.CrossEntropyLoss().to(self.device)
         
     def do_gradient_ops(self):
         # Gradient scale
@@ -69,16 +77,95 @@ class SpeakerEncoder(nn.Module):
         embeds = embeds_raw / torch.norm(embeds_raw, dim=1, keepdim=True)
         
         return embeds
-    """
-    def similarity_matrix(self, embeds):
+    
+    def load_model(self, weights_fpath):
+        """
+        Loads the model in memory. If this function is not explicitely called, it will be run on the 
+        first call to embed_frames() with the default weights file.
         
+        :param weights_fpath: the path to saved model weights.
+        """
+        checkpoint = torch.load(weights_fpath, map_location = self.device)
+        self.load_state_dict(checkpoint["model_state"])
+        
+        print("Loaded encoder \"%s\" trained to step %d" % (weights_fpath, checkpoint["step"]))
+        
+    def embed_frames_batch(self, frames_batch):
+        """
+        Computes embeddings for a batch of mel spectrogram.
+        
+        :param frames_batch: a batch mel of spectrogram as a numpy array of float32 of shape 
+        (batch_size, n_frames, n_channels)
+        :return: the embeddings as a numpy array of float32 of shape (batch_size, model_embedding_size)
+        """
+
+        frames = torch.from_numpy(frames_batch).to(self.device)
+        embed = self.forward(frames).detach().cpu()
+        return embed
+
+
+    
+    def embed_utterance(self, wav, using_partials=True, return_partials=False, **kwargs):
+        """
+        This is the main function!
+        Computes an embedding for a single utterance.
+        
+        # TODO: handle multiple wavs to benefit from batching on GPU
+        :param wav: a preprocessed (see audio.py) utterance waveform as a numpy array of float32
+        :param using_partials: if True, then the utterance is split in partial utterances of 
+        <partial_utterance_n_frames> frames and the utterance embedding is computed from their 
+        normalized average. If False, the utterance is instead computed from feeding the entire 
+        spectogram to the network.
+        :param return_partials: if True, the partial embeddings will also be returned along with the 
+        wav slices that correspond to the partial embeddings.
+        :param kwargs: additional arguments to compute_partial_splits()
+        :return: the embedding as a numpy array of float32 of shape (model_embedding_size,). If 
+        <return_partials> is True, the partial utterances as a numpy array of float32 of shape 
+        (n_partials, model_embedding_size) and the wav partials as a list of slices will also be 
+        returned. If <using_partials> is simultaneously set to False, both these values will be None 
+        instead.
+        """
+        # Load the wav from disk if needed
+        if isinstance(wav, str):
+            wav, source_sr = librosa.load(wav, sr=None)
+
+        # Process the entire utterance if not using partials
+        if not using_partials:
+            frames = wav_to_mel_spectrogram(wav)
+            embed = self.embed_frames_batch(frames[None, ...])[0]
+            if return_partials:
+                return embed, None, None
+            return embed
+        
+        # Compute where to split the utterance into partials and pad if necessary
+        wave_slices, mel_slices = compute_partial_slices(len(wav), **kwargs)
+        max_wave_length = wave_slices[-1].stop
+        if max_wave_length >= len(wav):
+            wav = np.pad(wav, (0, max_wave_length - len(wav)), "constant")
+        
+        # Split the utterance into partials
+        frames = wav_to_mel_spectrogram(wav)
+        frames_batch = np.array([frames[s] for s in mel_slices])
+        partial_embeds = self.embed_frames_batch(frames_batch)
+        
+        # Compute the utterance embedding from the partial embeddings
+        raw_embed = partial_embeds.mean(axis=0)
+        embed = raw_embed / np.linalg.norm(raw_embed, 2)
+        
+        if return_partials:
+            return embed, partial_embeds, wave_slices
+        return embed
+
+    def similarity_matrix(self, embeds):
+        '''
         Computes the similarity matrix according the section 2.1 of GE2E.
 
         :param embeds: the embeddings as a tensor of shape (speakers_per_batch, 
         utterances_per_speaker, embedding_size)
         :return: the similarity matrix as a tensor of shape (speakers_per_batch,
         utterances_per_speaker, speakers_per_batch)
-        
+        '''
+
         speakers_per_batch, utterances_per_speaker = embeds.shape[:2]
         
         # Inclusive centroids (1 per speaker). Cloning is needed for reverse differentiation
@@ -94,7 +181,7 @@ class SpeakerEncoder(nn.Module):
         # product of these vectors (which is just an element-wise multiplication reduced by a sum).
         # We vectorize the computation for efficiency.
         sim_matrix = torch.zeros(speakers_per_batch, utterances_per_speaker,
-                                 speakers_per_batch).to(self.loss_device)
+                                 speakers_per_batch).to(self.device)
         mask_matrix = 1 - np.eye(speakers_per_batch, dtype=np.int)
         for j in range(speakers_per_batch):
             mask = np.where(mask_matrix[j])[0]
@@ -103,7 +190,7 @@ class SpeakerEncoder(nn.Module):
         
         ## Even more vectorized version (slower maybe because of transpose)
         # sim_matrix2 = torch.zeros(speakers_per_batch, speakers_per_batch, utterances_per_speaker
-        #                           ).to(self.loss_device)
+        #                           ).to(self.device)
         # eye = np.eye(speakers_per_batch, dtype=np.int)
         # mask = np.where(1 - eye)
         # sim_matrix2[mask] = (embeds[mask[0]] * centroids_incl[mask[1]]).sum(dim=2)
@@ -115,13 +202,13 @@ class SpeakerEncoder(nn.Module):
         return sim_matrix
     
     def loss(self, embeds):
-        
+        '''
         Computes the softmax loss according the section 2.1 of GE2E.
         
         :param embeds: the embeddings as a tensor of shape (speakers_per_batch, 
         utterances_per_speaker, embedding_size)
         :return: the loss and the EER for this batch of embeddings.
-        
+        '''
         speakers_per_batch, utterances_per_speaker = embeds.shape[:2]
         
         # Loss
@@ -129,7 +216,7 @@ class SpeakerEncoder(nn.Module):
         sim_matrix = sim_matrix.reshape((speakers_per_batch * utterances_per_speaker, 
                                          speakers_per_batch))
         ground_truth = np.repeat(np.arange(speakers_per_batch), utterances_per_speaker)
-        target = torch.from_numpy(ground_truth).long().to(self.loss_device)
+        target = torch.from_numpy(ground_truth).long().to(self.device)
         loss = self.loss_fn(sim_matrix, target)
         
         # EER (not backpropagated)
@@ -143,4 +230,4 @@ class SpeakerEncoder(nn.Module):
             eer = brentq(lambda x: 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
             
         return loss, eer
-    """
+
