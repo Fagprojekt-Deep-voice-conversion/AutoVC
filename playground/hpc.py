8e72f7a74962a6c799fce7668fc05c36840e1099
Lukas Leindals <s183920@student.dtu.dk>
2022-01-04 15:49:17 +0100

    Update hpc.py

--- playground/hpc.py
+++ playground/hpc.py
@@ -1,16 +1,20 @@
-from paramiko import SSHClient
+from paramiko import SSHClient, AutoAddPolicy
 from getpass import getpass
 import time
-
+import sys, os
 
 class HPC:
     def __init__(self, hostname = 'login1.gbar.dtu.dk') -> None:
+        # get credentials 
+        self.hostname = hostname
+        self.__username = input("Username: ")
+        self.__pw = getpass("Password: ")
+
         # Connect
         self.client = SSHClient()
+        # self.client.set_missing_host_key_policy(AutoAddPolicy())
         self.client.load_system_host_keys()
-        username = input("Username: ")
-        pw = getpass("Password: ")
-        self.client.connect(hostname, username=username, password=pw)
+        self.client.connect(hostname, username=self.__username, password=self.__pw, port = '22')#, disabled_algorithms=dict(pubkeys=["rsa-sha2-512"]))
     
 
     def shell(self):
@@ -50,53 +54,68 @@ class HPC:
         # close connection
         self.close()
 
+    def send_data(self, data_path, receive_folder, gbar = True):
+        if gbar and "transfer.gbar.dtu.dk" not in receive_folder:
+            receive_folder = self.__username + "@transfer.gbar.dtu.dk:" + receive_folder
+  
+        if "win" in sys.platform:
+            command = f'pscp -pw "{self.__pw}" -r {data_path} {receive_folder}'
+        else:
+            command = f'rsync -r {data_path} {receive_folder}'
+
+        os.system(command)
+
+    def receive_data(self, data_path, receive_folder, gbar = True):
+        os.makedirs(receive_folder, exist_ok=True)
+
+        if gbar and "transfer.gbar.dtu.dk" not in data_path:
+            data_path = self.__username + "@transfer.gbar.dtu.dk:" + data_path
+
+        if "win" in sys.platform:
+            command = f'pscp -pw "{self.__pw}" -scp {data_path} {receive_folder}'
+        else:
+            command = f'rsync -r {data_path} {receive_folder}'
+
+        os.system(command)
+        
+
     def close(self):
         self.client.close()
 
 
 if __name__ == "__main__":
     HPC().shell()
+    # hpc = HPC()
+    # hpc.receive_data("Downloads/args.py", "playground/")
+    # hpc.send_data("playground/yang.wav", "Downloads")
+
+    # file = hpc.client.open_sftp()
+    # file.get("s183920@transfer.gbar.dtu.dk:Downloads/args.py", "playground/")
+    # file.close()
+
+    # hpc.close()
 
 
-# stdin, stdout, stderr = client.exec_command(command)
-            # print(f'STDOUT: {stdout.read().decode("utf8")}')
-            # print(f'STDERR: {stderr.read().decode("utf8")}')
+# stdin, stdout, stderr = self.client.exec_command(command)
+#         # stdin, stdout, stderr = self.client.exec_command('echo $PWD')
 
+#         print(type(stdin))  # <class 'paramiko.channel.ChannelStdinFile'>
+#         print(type(stdout))  # <class 'paramiko.channel.ChannelFile'>
+#         print(type(stderr))  # <class 'paramiko.channel.ChannelStderrFile'>
 
-# Run a command (execute PHP interpreter)
-# stdin, stdout, stderr = client.exec_command("; ".join([
-#     f"cd /work1/{username}/CoolData",
-#     "ls"
-# ]))
+#         # Optionally, send data via STDIN, and shutdown when done
+#         # stdin.write('<?php echo "Hello!"; sleep(2); ?>')
+#         # stdin.channel.shutdown_write()
 
+#         # Print output of command. Will wait for command to finish.
+#         print(f'STDOUT: {stdout.read().decode("utf8")}')
+#         print(f'STDERR: {stderr.read().decode("utf8")}')
 
-   
+#         # Get return code from command (0 is default for success)
+#         print(f'Return code: {stdout.channel.recv_exit_status()}')
 
-
-
-
-# stdin, stdout, stderr = client.exec_command('source CoolData-cluster-env/bin/activate')
-# stdin, stdout, stderr = client.exec_command('echo $PWD')
-
-# print(type(stdin))  # <class 'paramiko.channel.ChannelStdinFile'>
-# print(type(stdout))  # <class 'paramiko.channel.ChannelFile'>
-# print(type(stderr))  # <class 'paramiko.channel.ChannelStderrFile'>
-
-# Optionally, send data via STDIN, and shutdown when done
-# stdin.write('<?php echo "Hello!"; sleep(2); ?>')
-# stdin.channel.shutdown_write()
-
-# Print output of command. Will wait for command to finish.
-# print(f'STDOUT: {stdout.read().decode("utf8")}')
-# print(f'STDERR: {stderr.read().decode("utf8")}')
-
-# Get return code from command (0 is default for success)
-# print(f'Return code: {stdout.channel.recv_exit_status()}')
-
-# Because they are file objects, they need to be closed
-# stdin.close()
-# stdout.close()
-# stderr.close()
-
-# Close the client itself
+#         # Because they are file objects, they need to be closed
+#         stdin.close()
+#         stdout.close()
+#         stderr.close()
 
