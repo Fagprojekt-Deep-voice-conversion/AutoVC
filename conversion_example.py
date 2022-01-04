0b832a2c77645afec1c2f50bcb676061e896942d
Peter Grønning <s183922@win.dtu.dk>
2022-01-04 15:38:41 +0100

    Update conversion_example.py

--- conversion_example.py
+++ conversion_example.py
@@ -6,6 +6,7 @@ from autovc.vocoder.WaveRNN_model import WaveRNN
 from autovc.utils.hparams import hparams_waveRNN as hp
 import soundfile as sf
 import torch
+import numpy as np
 
 if __name__ == "__main__":
     # Models
