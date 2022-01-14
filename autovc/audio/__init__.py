fa978e7954b2f6c2771387dc558daffcb47d1db3
Lukas Leindals <s183920@student.dtu.dk>
2022-01-14 12:25:01 +0100

    load, save and resample to audio class

--- autovc/audio/__init__.py
+++ autovc/audio/__init__.py
@@ -1,3 +1,68 @@
+"""
+A class for handling audio data
+"""
+
+import librosa
+import soundfile as sf
+
 class Audio:
-    def __init__(self) -> None:
-        self.a = 1
+    def __init__(self, wav, sr = 22050, sr_org = None) -> None:
+        """
+        Initlialises the audio class
+
+        Parameters
+        ----------
+        wav:
+            a path to a wav file or a np array with audio data
+        sr:
+            the samplerate to give the audio data (librosa.resample is used for this)
+            if None, no resampling is done
+        sr_org:
+            the samplerate of the given audio data, this is only necesary if wav is not a path
+        """
+
+        # load file
+        if isinstance(wav, str):
+            self.wav_path = wav
+            self.wav, self.sr_org = librosa.load(self.wav_path, sr=sr_org)
+            # self.sr = self.sr_org
+            # self.wav = librosa.resample(self.wav, self.sr_org, self.sr_trg)
+            # self.wav = self.resample()
+        else:
+            assert sr_org is not None, "sr_org must be given if wav is not a file path"
+            self.wav_path = None
+            self.sr_org = sr_org
+            self.wav = wav
+        
+
+        # resample audio to target sr
+        self.wav_org = self.wav.copy() # store orginal wav (to use if wav has been changed for the worse)
+        self.sr = self.sr_org # sr is set to origninal sampling rate
+        if sr is not None:
+            self.wav = self.resample(sr)
+            self.sr = sr # this is now the new sampling rate
+        
+        
+
+    def save(self, save_path = "example_audio.wav", sr = None):
+        """
+        Saves the audio data to the given path with the given sampling rate (sr). Sampling rate is set objects value if None is given
+        """
+        sr = self.sr if sr is None else sr
+        sf.write(save_path, self.resample(sr), samplerate=sr)
+        
+
+    def resample(self, sr):
+        """
+        Resamples self.wav with new sample rate
+        """
+        if sr == self.sr:
+            return self.wav
+        else:
+            return librosa.resample(self.wav, self.sr, sr)
+
+
+if __name__ == "__main__":
+    wav = Audio("data/samples/chooped7.wav")
+    print(wav.sr, wav.sr_org)
+    wav.save()
