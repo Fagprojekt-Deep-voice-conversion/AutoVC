72b889e8dde95ce2f5a670fb6e4c3213ce5485ab
Lukas Leindals <s183920@student.dtu.dk>
2022-01-14 14:00:20 +0100

    create skeleton

---
+++ autovc/audio/spectrogram.py
@@ -0,0 +1,25 @@
+"""
+Some useful tools for spectrograms. 
+"""
+
+
+def normalize_spec():
+    pass
+
+def denormalize_spec():
+    pass
+
+def amp_to_db():
+    pass
+
+def db_to_amp():
+    pass
+
+def audio_to_melspectrogram():
+    pass
+
+def compute_partial_slices():
+    pass
+
+def split_melspectrograms():
+    pass
---
+++ autovc/audio/tools.py
@@ -0,0 +1,24 @@
+"""
+Some useful tools to use on audio files. 
+To use the tools load an audio file with librosa.load() or the Audio class provided (`from autovc import Audio`)
+"""
+
+
+def create_silence_mask():
+    pass
+
+def trim_long_silences():
+    pass
+
+def split_audio():
+    pass
+
+def combine_audio():
+    pass
+
+def normalize_volume():
+    pass
+
+def remove_noise():
+    pass
+
