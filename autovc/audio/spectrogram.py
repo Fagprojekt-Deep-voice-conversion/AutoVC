a93af71154ec76030a0cdcfb6696e96669af6d53
Lukas Leindals <s183920@student.dtu.dk>
2022-01-14 17:09:06 +0100

    compute_partial_slices

--- autovc/audio/spectrogram.py
+++ autovc/audio/spectrogram.py
@@ -70,8 +70,68 @@ def db_to_amp(power):
 def audio_to_melspectrogram():
     pass
 
-def compute_partial_slices():
-    pass
+def compute_partial_slices(n_samples, sr, partial_utterance_n_frames = 160,
+                           min_pad_coverage = 0.75,
+                           overlap = 0.5,
+                           mel_window_step = 10):
+    """
+    Computes where to split an utterance waveform and its corresponding mel spectrogram to obtain 
+    partial utterances of <partial_utterance_n_frames> each. Both the waveform and the mel 
+    spectrogram slices are returned, so as to make each partial utterance waveform correspond to 
+    its spectrogram. 
+    This function assumes that the mel spectrogram parameters used are those 
+    defined in params_data.py.
+    
+    The returned ranges may be indexing further than the length of the waveform. It is 
+    recommended that you pad the waveform with zeros up to wave_slices[-1].stop.
+    
+    Parameters
+    ----------
+    n_samples: 
+        The number of samples in the waveform
+    partial_utterance_n_frames: 
+        The number of mel spectrogram frames in each partial utterance (x*10 ms)
+    min_pad_coverage: 
+        When reaching the last partial utterance, it may or may not have 
+        enough frames. If at least <min_pad_coverage> of <partial_utterance_n_frames> are present, 
+        then the last partial utterance will be considered, as if we padded the audio. 
+        Otherwise, it will be discarded, as if we trimmed the audio. If there aren't enough frames for 1 partial 
+        utterance, this parameter is ignored so that the function always returns at least 1 slice.
+    overlap: 
+        By how much the partial utterance should overlap. If set to 0, the partial utterances are entirely disjoint. 
+    mel_window_step:
+        How large each frame should be (in ms).
+    Return
+    ------
+    wav_slices:
+        The waveform slices as lists of array slices. Index the waveform with these slices to obtain the partial utterances.
+    mel_slices:
+        Mel spectrogram slices as lists of array slices. Index the mel spectrogram with these slices to obtain the partial utterances.
+    """
+    assert 0 <= overlap < 1
+    assert 0 < min_pad_coverage <= 1
+    
+    samples_per_frame = int((sr * mel_window_step / 1000))
+    n_frames = int(np.ceil((n_samples + 1) / samples_per_frame))
+    frame_step = max(int(np.round(partial_utterance_n_frames * (1 - overlap))), 1)
+
+    # Compute the slices
+    wav_slices, mel_slices = [], []
+    steps = max(1, n_frames - partial_utterance_n_frames + frame_step + 1)
+    for i in range(0, steps, frame_step):
+        mel_range = np.array([i, i + partial_utterance_n_frames])
+        wav_range = mel_range * samples_per_frame
+        mel_slices.append(slice(*mel_range))
+        wav_slices.append(slice(*wav_range))
+        
+    # Evaluate whether extra padding is warranted or not
+    last_wav_range = wav_slices[-1]
+    coverage = (n_samples - last_wav_range.start) / (last_wav_range.stop - last_wav_range.start)
+    if coverage < min_pad_coverage and len(mel_slices) > 1:
+        mel_slices = mel_slices[:-1]
+        wav_slices = wav_slices[:-1]
+    
+    return wav_slices, mel_slices
 
 def split_melspectrograms():
     pass
