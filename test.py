5bbb7cc8655a3992f46545bdc351864a2e0d8028
Lukas Leindals <s183920@student.dtu.dk>
2022-01-17 10:57:55 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from autovc.utils.audio import get_mel_frames, audio_to_melspectrogram
-from autovc.utils.model_loader import load_model
-import torch
-
-
-AE = load_model('auto_encoder', 'models/AutoVC/AutoVC_SMK.pt')
-
-print(torch.stack(get_mel_frames('data/conversions/hej.wav', audio_to_melspectrogram, sr = 22000, order = 'MF')).shape)
-
