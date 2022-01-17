4a0f0244e2e046d03dac790cb341563443d1a5a3
Peter Grønning <s183922@student.dtu.dk>
2022-01-17 16:45:30 +0100

    Test stuff

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 from autovc.utils.audio import get_mel_frames, audio_to_melspectrogram
 from autovc.utils.model_loader import load_model
 import torch
-
-
+import numpy as np
+import matplotlib.pyplot as plt
 AE = load_model('auto_encoder', 'models/AutoVC/AutoVC_SMK.pt')
+SE = load_model('speaker_encoder', 'models/SpeakerEncoder/SpeakerEncoder.pt')
+N = 160
+frames = get_mel_frames('data/conversions/hej.wav', audio_to_melspectrogram, sr = 22050, mel_window_step = 12.5, order = 'MF', partial_utterance_n_frames = N)
+T = torch.stack(frames)
+emb = torch.randn((T.size(0), 256))
 
-print(torch.stack(get_mel_frames('data/conversions/hej.wav', audio_to_melspectrogram, sr = 22000, order = 'MF')).shape)
+_, out, _ = AE(T, emb, emb)
 
+window = np.hanning(N)
