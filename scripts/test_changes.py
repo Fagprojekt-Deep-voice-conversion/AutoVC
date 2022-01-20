495a4fab05935a9fc4fd229394a9cb3c2762eca4
Lukas Leindals <s183920@student.dtu.dk>
2022-01-20 13:19:23 +0100

    Update test_changes.py

--- scripts/test_changes.py
+++ scripts/test_changes.py
@@ -4,20 +4,17 @@
 from autovc import VoiceConverter
 from autovc.utils.model_loader import load_model
 
-vc = VoiceConverter(wandb_params = {"mode" : "disabled"}, speaker_encoder = "models/SpeakerEncoder/SpeakerEncoder_SMK.pt")
+vc = VoiceConverter(wandb_params = {"mode" : "disabled"}, speaker_encoder = "models/SpeakerEncoder/SpeakerEncoder.pt")
 
 print("Testing Auto Encoder training...")
-# vc.train(n_epochs = 1, data_path = ["data/samples/mette_183.wav", "data/samples/chooped7.wav"], conversion_examples = False)
-vc.train(n_epochs = 1, data_path = ["data/yang_long.wav", "data/samples/chooped7.wav"], conversion_examples = False, auto_encoder_params = {"cut" : True, "speakers" : True})
+vc.train(n_epochs = 1, data_path = ["data/samples/mette_183.wav", "data/samples/chooped7.wav"], conversion_examples = False)
+# vc.train(n_epochs = 1, data_path = ["data/yang_long.wav", "data/samples/chooped7.wav"], conversion_examples = False, auto_encoder_params = {"cut" : True, "speakers" : True})
 
 print("Testing conversion...")
 vc.convert("data/samples/mette_183.wav", "data/samples/chooped7.wav", out_name = "conversion_test.wav")
 
+
 print("Testing speaker encoder...")
-SE = load_model('speaker_encoder', 'models/SpeakerEncoder/SpeakerEncoder.pt')
-# SE.learn_speaker('hilde', 'data/test_data/hilde_7sek')
-# SE.learn_speaker('yang', 'data/test_data/HaegueYang_10sek')
-# SE.save('SpeakerEncoder2.pt')
-# SE = load_model('speaker_encoder', 'SpeakerEncoder2.pt')
-SE.learn_speaker("hilde", ["data/samples/hilde_1.wav", "data/samples/hilde_301.wav"])
-print(SE.speakers)
+vc.SE.learn_speaker("yang", ["data/samples/chooped7.wav", "data/samples/HaegueSMK.wav", "data/samples/HaegueYang_5.wav"])
+print(vc.SE.speakers)
+vc.convert("data/samples/mette_183.wav", "yang", out_name = "conversion_test2.wav")
